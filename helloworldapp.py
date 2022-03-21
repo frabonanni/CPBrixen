5e2927f330acc80664227ccff6dd659fed3eb0c8
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-03-21 16:32:18 +0100

    Update helloworldapp.py

--- helloworldapp.py
+++ helloworldapp.py
@@ -14,7 +14,7 @@ import json, requests
 
 #add your own APIkey
 APIkey = '6e1007dd818bd4d4dbd95b99a1ff01c7'
-location = st.text_input('London')
+location = st.text_input('Which city temperature?','London')
 
 
 #check API documentation to see what structure of URL is needed to access the data
