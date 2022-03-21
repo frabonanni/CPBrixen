2fb74850d686073656cb82a6dba4f26b5bff0bba
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-03-21 15:48:03 +0100

    Update helloworldapp.py

--- helloworldapp.py
+++ helloworldapp.py
@@ -2,8 +2,9 @@ import streamlit as st
 st.header("hello world")
 title = st.text_input('Gimme a movie title', 'Meet Joe Black', max_chars=7)
 st.write('The current movie title is', title)
+
 genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'))
 if genre == 'Comedy':
      st.write('You selected comedy.')
-   else:
+else:
      st.write("You didn't select comedy.")
