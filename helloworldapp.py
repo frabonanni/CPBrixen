ca9abb42552e22ec365226fc19b56a018be5e238
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-03-21 16:13:11 +0100

    Update helloworldapp.py

--- helloworldapp.py
+++ helloworldapp.py
@@ -8,3 +8,32 @@ if genre == 'Comedy':
      st.write('You selected comedy.')
 else:
      st.write("You didn't select comedy.")
+ # ! python3
+import json, requests 
+
+# add your own APIkey
+APIkey = '6e1007dd818bd4d4dbd95b99a1ff01c7'
+location = 'london'
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+st.text(weatherData['main']['temp_max']) 
+# more???????????    
+     
