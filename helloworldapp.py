5e357bb88b4562d020a9ac8045917aa04d6eccb8
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-03-21 15:52:05 +0100

    Update helloworldapp.py

--- helloworldapp.py
+++ helloworldapp.py
@@ -3,7 +3,7 @@ st.header("hello world")
 title = st.text_input('Gimme a movie title', 'Meet Joe Black', max_chars=7)
 st.write('The current movie title is', title)
 
-genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'))
+genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'), help= 'click one of the three options')
 if genre == 'Comedy':
      st.write('You selected comedy.')
 else:
