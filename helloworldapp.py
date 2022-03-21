275348382985dc899c1720a83e8a3778a5d222f5
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-03-21 15:43:45 +0100

    Update helloworldapp.py

--- helloworldapp.py
+++ helloworldapp.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header("hello world")
-title = st.text_input('Gimme a movie title', 'Meet Joe Black')
+title = st.text_input('Gimme a movie title', 'Meet Joe Black', max_chars=7)
 st.write('The current movie title is', title)
