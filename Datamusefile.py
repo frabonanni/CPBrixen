53314c71619edad846cd5dcb5a67fe40f019422d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-03-28 16:35:07 +0200

    Update Datamusefile.py

--- Datamusefile.py
+++ Datamusefile.py
@@ -2,28 +2,28 @@
 import json,requests
 from pprint import pprint
 
-keyword=input('plz give me a keyword')
+keyword=input_text('plz give me a keyword')
 option = st.selectbox('Do you prefer synonyms or antonyms of the word you chose?',('synonym', 'antonym'))
 st.write('You selected:', option)
 
+url1= 'https://api.datamuse.com/words?rel_syn=' + keyword + '&max=5'
+url2 = 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=5'
 if option == 'synonym':
    url = url1
 else:
    url = url2
-url1= 'https://api.datamuse.com/words?rel_syn=' + keyword + '&max=5'
-url2 = 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=5'
+
 
 
 #Step3: Download the JSON data from the API.
-response1 = requests.get(url1) 
-response2 = requests.get(url2) 
+response = requests.get(url)  
 #Uncomment to see the raw JSON text:
 #print(response.text)  
 
 
 #Step4: Load JSON data into a Python variable and use it in your program.
-dataFromDatamuse1 = json.loads(response1.text)
-dataFromDatamuse2 = json.loads(response2.text)
+dataFromDatamuse = json.loads(response.text)
+
 #print(dataFromDatamuse1)
 #print(dataFromDatamuse2)
 
@@ -32,5 +32,5 @@ dataFromDatamuse2 = json.loads(response2.text)
 #print(dataFromDatamuse) 
 #Uncomment to see a better readable version:
 #pprint(dataFromDatamuse) #dont forget to import the correct pprint library to make this work
-pprint(dataFromDatamuse1[0:4])#if you just want to see the first 9 results
-pprint(dataFromDatamuse2[0:4])
+pprint(dataFromDatamuse[0:4])#if you just want to see the first 9 results
+
