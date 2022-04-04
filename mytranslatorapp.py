9b82ca9962bb9d818ab1403a0050700659e70c8d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-04-04 16:18:15 +0200

    Update mytranslatorapp.py

--- mytranslatorapp.py
+++ mytranslatorapp.py
@@ -1,1 +1,5 @@
 from googletrans import Translator
+translator = Translator()
+
+word = st.text_input('Gimme a word to translate ')
+destlang= st.text_input('Tell me a two letter code for the destination language like es or en: ')
