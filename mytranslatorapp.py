d9227e56ec7ecf6974f79fac77bda833031e01b8
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-04-04 17:01:16 +0200

    Update mytranslatorapp.py

--- mytranslatorapp.py
+++ mytranslatorapp.py
@@ -2,7 +2,7 @@ from googletrans import Translator
 import streamlit as st
 translator = Translator()
 
-st.header('my cool translator')
+st.header('my cool translatorxD')
 srclang = st.text_input('tell me the source language')
 word = st.text_input('Gimme a word to translate ')
 destlang= st.text_input('Tell me a two letter code for the destination language like es or en: ')
