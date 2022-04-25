6e192eeb5f992d4f7995135131f94d6963c68a6d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-04-25 10:58:39 +0200

    Update MediaWiki.py

--- MediaWiki.py
+++ MediaWiki.py
@@ -1,5 +1,5 @@
 import json, requests
-from streamlit import st
+import streamlit as st
 from pprint import pprint
 
 url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=art&srlimit=3'
@@ -13,9 +13,9 @@ soup = BeautifulSoup(textwithHTMLtags)
 textwithoutHTMLtags = soup.get_text()
 st.write(textwithoutHTMLtags)
 
-keyword= st.text_input('plz give me a keyword', 'word')
+keyword= st.text_input('plz give me a keyword', 'art')
 option = st.selectbox('What you want to know about?')
-st.write('')
+st.write('ART is a diverse range of (the products of) human activities involving the conscious use of creative imagination to express technical proficiency, beauty')
 
 response = requests.get(url)
 dataFromWikipedia = json.loads(response.text)
