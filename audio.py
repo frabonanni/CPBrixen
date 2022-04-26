7bc6b4b381d966a98861a6a43fae4d104387cf43
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-04-26 11:47:02 +0200

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,1 +1,17 @@
 import streamlit as st
+from gtts import gTTS
+import IPython.display as ipd
+from googletrans import Translator
+
+new_text = st.input('give me a text: ')
+dest_lan = st.input('give me a language: ')
+translator = Translator()
+transToLan = translator.translate(new_text, dest= dest_lan)
+
+ppp = transToLan.text
+print(ppp)
+file_name = st.input('give me a name with .mp3: ')
+new_name=gTTS(text= ppp, lang= dest_lan)
+new_name.save(file_name)
+
+st.audio(ipd.Audio(file_name))
