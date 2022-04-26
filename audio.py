06ebce18a91ff7e2730213c88c896a9032053150
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-04-26 12:49:41 +0200

    Update audio.py

--- audio.py
+++ audio.py
@@ -3,17 +3,17 @@ from gtts import gTTS
 
 from googletrans import Translator
 
-if new_text != '':
-    new_text = st.input()
-if dest_lan != '':
-    dest_lan = st.input()
+
+new_text = st.input()
+
+dest_lan = st.input()
 translator = Translator()
 transToLan = translator.translate(new_text, dest= dest_lan)
 
 ppp = transToLan.text
 print(ppp)
-if file_name != '':
-    file_name = st.input()
+
+file_name = st.input()
 new_name=gTTS(text= ppp, lang= dest_lan)
 new_name.save(file_name)
 audio_file = open(file_name, "rb")
