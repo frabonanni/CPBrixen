9ab068d66ab49d7934fccd854222ec3f1f393779
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-03 10:53:37 +0200

    Update audio.py

--- audio.py
+++ audio.py
@@ -13,12 +13,13 @@ if new_text != ' ':
     ppp = transToLan.text
 
 
-
     tts1=gTTS(text= ppp, lang= dest_lan)
     tts1.save('file_name.mp3')
     audio_file = open('file_name.mp3', "rb")
     st.audio(data=audio_file, format="audio/mp3", start_time=0)
 
+   st.download_button(label= "download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3") 
 else:
     pass
+
 
