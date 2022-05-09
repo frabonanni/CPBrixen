a362b2a892559af74a3e08cffab28bbbb9a8bb8d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-09 15:55:58 +0200

    Update and rename MyAppproject.py to project/MyAppproject.py

---
+++ project/MyAppproject.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import speech_recognition as sr
+
+r = sr.Recognizer()
+uploaded_file = st.file_uploader("Choose a file")
+if AUDIO_FILE is not None:
+  with sr.AudioFile(AUDIO_FILE) as source:
+      audio = r.record(source)  
+  recognised_text= r.recognize_google(audio)
+  st.text('the text recognized from the audio seems to be: ')
+  st.text( recognised_text)
+
+
