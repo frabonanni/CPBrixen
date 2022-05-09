49bfa35a90ce6757d9ecffc18cf1968fde63969d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-09 15:29:12 +0200

    Update recog.py

--- recog.py
+++ recog.py
@@ -1,1 +1,15 @@
+import speech_recognition as sr
+
+
+r = sr.Recognizer()
+AUDIO_FILE = "sample_audio_short.wav"   #mp3 files are not supported
+#AUDIO_FILE = "sample_audio_long.wav"    #these files are in files-section of Class08 folder, on MS Teams.
+if AUDIO_FILE is not None:
+  
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+recognised_text= r.recognize_google(audio)
+
+st.text('the text recognized from the audio seems to be: ')
+st.text( recognised_text)
 
