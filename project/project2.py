dd53a3d4f20184feaff288db30fb62886798b0bf
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-10 17:30:53 +0200

    Update project2.py

--- project/project2.py
+++ project/project2.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 uploaded_file = st.file_uploader("Choose a file")
-if AUDIO_FILE is not None:
+if upload_file is not None:
   with sr.AudioFile(AUDIO_FILE) as source:
       audio = r.record(source)  
   recognised_text= r.recognize_google(audio)
