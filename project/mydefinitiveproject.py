68708a7d979626b0cb530702168ba57a9d7a5107
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-21 09:17:34 +0200

    Update mydefinitiveproject.py

--- project/mydefinitiveproject.py
+++ project/mydefinitiveproject.py
@@ -1,32 +1,34 @@
 import streamlit as st
-import speech_recognition as sr
-
 import os
-
+!pip install SpeechRecognition
+import speech_recognition as sr
+!pip install pydub
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+AUDIO_FILE = "/content/drive/MyDrive/Speech_Technology/Audios/speaker_female_1.wav"
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+    print("Recognized Speech: \n" + r.recognize_google(audio))
+!pip install ibm_watson
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+apikey='FoQQPWQYE0B-3i38r1IbI_20uzPYHiF8JMHND1DuQ6yG' ## qui ci va il tuo Apikey che crei nel profilo
+url='https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/36876297-be6a-479d-9f8a-f34ef6a63c31' # il tuo url
 
-def silence_based_conversation(path):
-    song = AudioSegment.from_wav(path)
-    fh= open("recognized.txt", "w+")
-    # split track where silence is 0.5 seconds 
-    # or more and get chunks
-    chunks = split_on_silence(song,silence_thresh = -16)
-    i = 0
-    for chunk in chunks:
-        chunk_silent= AudioSegment.silent(duration = 10)
-        audio_chunk.export("./chunk{0}.wav".format(i), bitrate ='192k', format ="wav")
-        filename = 'chunk'+str(i)+'.wav'
-  
-        st.write("Processing chunk "+str(i))                  
-                            
-        r = sr.Recognizer()
-        
-        with sr.AudioFile(filename) as source:
-             audio = r.record(source)  
-             recognised_text= r.recognize_google(audio)
-             st.text('the text recognized from the audio seems to be: ')
-             st.text( recognised_text)
-audiofile = st.file_uploader("Upload wav file")
-if audiofile is not None:
-    silence_based_conversation(audiofile)
+authenticator = IAMAuthenticator(apikey)
+stt = SpeechToTextV1(authenticator=authenticator)
+
+stt.set_service_url(url)
+stt.set_disable_ssl_verification(True)
+with open("/content/drive/MyDrive/Speech_Technology/Audios/Es_andalucian_audios/dani2.wav", 'rb') as f:  
+  res = stt.recognize(audio=f, content_type='audio/wav', model ='es-ES_NarrowbandModel').get_result()
+
+res
+
+for el in res["results"]:
+  for l in el["alternatives"]:
+    st.write(l["transcript"])
+   audio_text= 
+for el["results"] in res['results'][0]['alternatives'][0]['transcript']
+st.write(audio_text)
