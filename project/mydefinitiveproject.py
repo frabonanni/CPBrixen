a2bb5c8970e85a84772bb5e323587fb5c5110ac6
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-21 15:03:01 +0200

    Update mydefinitiveproject.py

--- project/mydefinitiveproject.py
+++ project/mydefinitiveproject.py
@@ -5,7 +5,7 @@ st.header('Do you not know these words? Let me help you understand them')
 #pip install pydub
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-AUDIO_FILE = "/content/drive/MyDrive/Speech_Technology/Audios/speaker_female_1.wav"
+AUDIO_FILE = "/content/drive/MyDrive/registrazione python 1 (online-audio-converter.com).wav"
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)  # read the entire audio file
@@ -19,7 +19,7 @@ stt = SpeechToTextV1(authenticator=authenticator)
 
 stt.set_service_url(url)
 stt.set_disable_ssl_verification(True)
-with open("/content/drive/MyDrive/Speech_Technology/Audios/Es_andalucian_audios/dani2.wav", 'rb') as f:  
+with open("/content/drive/MyDrive/registrazione python 1 (online-audio-converter.com).wav", 'rb') as f:  
   res = stt.recognize(audio=f, content_type='audio/wav', model ='es-ES_NarrowbandModel').get_result()
 
 res
