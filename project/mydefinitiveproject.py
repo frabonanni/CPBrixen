2534c468871324bdab108e806f994a7295df844f
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-21 14:30:15 +0200

    Update mydefinitiveproject.py

--- project/mydefinitiveproject.py
+++ project/mydefinitiveproject.py
@@ -27,6 +27,6 @@ res
 for el in res["results"]:
   for l in el["alternatives"]:
     st.write(l["transcript"])
-    audio_text= 
+audio_text= 
 for el["results"] in res['results'][0]['alternatives'][0]['transcript']:
     st.write(audio_text)
