53c0b27439de3ebf7b0b9d53e9477464187a2372
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-29 20:59:25 +0200

    Update MyAppproject.py

--- project/MyAppproject.py
+++ project/MyAppproject.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from PIL import Image
+image1 = Image.open('language-translator-translater.jpg')
+st.image(image1, caption = "Translate words")
 import os
 import speech_recognition as sr
 st.title('The ILT App')
 st.write(""" -Hello user! This is the ILT app. It is very simple to use and it will help you understand some useful words in a particular situation or context""")
 st.write(""" -Please listen to the first recording, read the text below it and then listen to the second audio file""")
-image1 = Image.open('language-translator-translater.jpg')
-st.image(image1, caption = "Translate words")
+
 #pip install pydub
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
