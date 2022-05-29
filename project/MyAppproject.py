3f9608625e2ddc24d3d178687fcc3a6519100377
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-29 20:56:44 +0200

    Update MyAppproject.py

--- project/MyAppproject.py
+++ project/MyAppproject.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import os
 import speech_recognition as sr
-st.title('LawWiki translator for foreigners')
+st.title('The ILT App')
 st.write(""" -Hello user! This is the ILT app. It is very simple to use and it will help you understand some useful words in a particular situation or context""")
 st.write(""" -Please listen to the first recording, read the text below it and then listen to the second audio file""")
 image1 = Image.open('language-translator-translater.jpg')
