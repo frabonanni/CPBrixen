69842c7215f672a364af5525f6c69d13350251d9
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-31 10:39:53 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -3,6 +3,7 @@ import json,requests
 from pprint import pprint
 from gtts import gTTS
 from googletrans import Translator
+import IPython.display as ipd
 
 st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
@@ -13,3 +14,4 @@ st.write('You selected:', option)
 
 word1= gTTS(text='permesso di soggiorno')
 word1.save('english_residence permit.mp3')
+ipd.display(ipd.Audio('english_residence permit.mp3', autoplay=True))
