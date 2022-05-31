032eb0157f6ac4e3a0575cf24b8383a817d7124b
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-31 11:38:27 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -9,7 +9,6 @@ st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
 word = st.text_input('Gimme a word to translate ')
 destlang= st.text_input('Tell me a two letter code for the destination language like es or en: ')
-abc = translator.translate(word, src = srclang , dest= destlang)
 st.write('the translation is',abc.text)
 st.file_uploader(label= "upload new_text", type=None, accept_multiple_files=False)
 
