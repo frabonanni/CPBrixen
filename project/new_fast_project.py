bbb257fb6be6904062de0b1ef35dfaa1cafa000d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-31 11:30:54 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -9,20 +9,5 @@ st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
 
 new_text = st.text_input('give me a text:')
-dest_lan = st.text_input('give me a 2-letter code:', 'it')
-
-if new_text != ' ':
-    translator = Translator()
-    transToLan = translator.translate(new_text, dest= dest_lan)
-    ppp = transToLan.text
-
-
-    tts1=gTTS(text= ppp, lang= dest_lan)
-    tts1.save('file_name.mp3')
-    audio_file = open('file_name.mp3', "rb")
-    st.audio(data=audio_file, format="audio/mp3", start_time=0)
-
-    st.download_button(label= "download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
-else:
-    pass
-
+word1=gTTS(text='permesso di soggiorno')
+word1.save('italian_permesso_soggiorno')
