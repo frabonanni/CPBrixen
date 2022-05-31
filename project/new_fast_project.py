22ee1f4c62a6c6cecaa065bbb3b722d03fbc7e8d
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-05-31 11:35:09 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -8,6 +8,11 @@ import IPython.display as ipd
 st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
 
-new_text = st.text_input('give me a text:')
+new_text = st.text_input('give me a text:','permesso di soggiorno')
+new_text = st.text_input('give me a text:','ciao')
+src_lan = st.text_input('give me a 2-letter code:', 'it')
+st.file_uploader(label= "upload new_text", type=None, accept_multiple_files=False)
+
+
 word1=gTTS(text='permesso di soggiorno')
 word1.save('italian_permesso_soggiorno')
