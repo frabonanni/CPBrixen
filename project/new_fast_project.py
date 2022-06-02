4b60a10d3e53510793924cc49823e241ff8490bf
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-02 13:39:19 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -15,13 +15,10 @@ if word != ' ':
    TranstoLan= translator.translate(word, dest= destlang)
    ppp= TranstoLan.text                        
    st.write('the translation is',TranstoLan.text)
-
-
-
-word1=gTTS(text=ppp, lang =destlang)
-word1.save('file_name.mp3')
-audio_file= open('file_name.mp3', "rb")
-st.audio(data=audio_file, format="audio/mp3", start_time=0)
-st.download_button(label= "download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
+   word1=gTTS(text=ppp, lang =destlang)
+   word1.save('file_name.mp3')
+   audio_file= open('file_name.mp3', "rb")
+   st.audio(data=audio_file, format="audio/mp3", start_time=0)
+   st.download_button(label= "download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
 else:
    pass                        
