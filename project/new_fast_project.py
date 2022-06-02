d01bc62947a302066c6e2972703999453ef623ee
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-02 14:17:07 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from gtts import gTTS
 from googletrans import Translator
 import IPython.display as ipd
-From PIL import Image
+from PIL import Image
 
 st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
