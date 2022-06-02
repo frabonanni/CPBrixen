47f12592e6b3b7f19bba96f9704496fe714a3a1c
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-02 14:26:38 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -9,16 +9,16 @@ from PIL import Image
 st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
 st.image("https://raw.githubusercontent.com/frabonanni/CPBrixen/main/project/language-translator-translater.jpg")
-word = st.text_input('Give me a word to translate ')
+myword = st.text_input('Give me a word to translate ')
 srclang= 'it'
 destlang= st.text_input('Tell me a two letter code for the destination language like es or en: ')
-if word != ' ':
+if myword != ' ':
    translator= Translator()
-   TranstoLan= translator.translate(word, src=srclang, dest= destlang)
+   TranstoLan= translator.translate(myword, src=srclang, dest= destlang)
    ppp= TranstoLan.text                        
    st.write('the translation is',TranstoLan.text)
-   word1=gTTS(text=ppp, lang =destlang)
-   word1.save('file_name.mp3')
+   firstword=gTTS(text=ppp, lang =srclang)
+   firstword.save('file_name.mp3')
    audio_file= open('file_name.mp3', "rb")
    st.audio(data=audio_file, format="audio/mp3", start_time=0)
    st.download_button(label= "download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
