4166ae2c4eb62b341224c4ad165536d674cf56e2
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-02 12:27:30 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -8,7 +8,7 @@ import IPython.display as ipd
 st.title("MyWiki")
 st.header('Translate Italian words to any language you want')
 word = st.text_input('Give me a word to translate ')
-destlang= st.text_input('Tell me a two letter code for the destination language like es or en: '
+destlang= st.text_input('Tell me a two letter code for the destination language like es or en: ')
 st.file_uploader(label= "upload new_text", type=None, accept_multiple_files=False)                        
 
 if word != ' ':
