7ec736c9660c8a42177190aebe988f613dad9392
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-10 23:10:21 +0200

    Update MyTransITproject.py

--- project/MyTransITproject.py
+++ project/MyTransITproject.py
@@ -50,8 +50,8 @@ if destlang is not None and myword is not '':
    ppp= TranstoLan.text
    st.write('the translation is',TranstoLan.text)
    
-   yourtext= gTTS(text= cleanTextfromWikipedia)
-   yourtext.save('your_translation.mp3')
+   yourtext=gTTS(text= cleanTextfromWikipedia)
+   yourtext.save('yourtranslation.mp3')
    audio_file = open('file_name.mp3', "rb")
    st.audio(data=audio_file, format="audio/mp3", start_time=0)
                       
