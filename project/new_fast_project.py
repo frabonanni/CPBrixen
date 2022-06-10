8515f9b432bf7f856682dd40dd562d126ad2158f
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-10 18:06:44 +0200

    Update new_fast_project.py

--- project/new_fast_project.py
+++ project/new_fast_project.py
@@ -7,36 +7,46 @@ import IPython.display as ipd
 from PIL import Image
 from lxml import html
 
+
+def strip_html(stringwithHTML):
+    mydefinition = str(html.fromstring(stringwithHTML).text_content())
+    return str(html.fromstring(stringwithHTML).text_content())
+
+
+
+
 st.title("TradFromIT")
 st.header('Translate Italian words to any language you want')
 st.image("https://raw.githubusercontent.com/frabonanni/CPBrixen/main/project/language-translator-translater.jpg")
 myword = st.text_input('Give me a word to translate ')
 srclang= 'it'
 destlang= st.text_input('Tell me a two letter code for the destination language like es or en: ')
-if myword != ' ':
-   translator= Translator()
-   TranstoLan= translator.translate(myword, src=srclang, dest= destlang)
-   ppp= TranstoLan.text                        
-   st.write('the translation is',TranstoLan.text)
+if destlang is not None:
    firstword=gTTS(text=myword, lang =srclang)
    firstword.save('file_name.mp3')
    audio_file= open('file_name.mp3', "rb")
    st.audio(data=audio_file, format="audio/mp3", start_time=0)
    st.download_button(label= "download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
+   
+   translator= Translator()
+   TranstoLan= translator.translate(myword, src=srclang, dest= destlang)
+   ppp= TranstoLan.text                        
+   st.write('the translation is',TranstoLan.text)
+   
+   mykeyword = myword
+   url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+mykeyword
+   response = requests.get(url)
+   dataFromWikipedia = json.loads(response.text)
+   textfromWikipedia = dataFromWikipedia['query']['search'][0]['snippet']
+   cleanTextfromWikipedia = strip_html(textfromWikipedia)    
+   st.write(cleanTextfromWikipedia)
+   
+   translator= Translator()
+   TranstoLan= translator.translate(cleanTextfromWikipedia, src='en', dest= destlang)
+   ppp= TranstoLan.text                        
+   st.write('the translation is',TranstoLan.text)
+      
+   
 else:
    pass                        
 
-
-mykeyword = myword
-url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+mykeyword
-response = requests.get(url)
-dataFromWikipedia = json.loads(response.text)
-textfromWikipedia = dataFromWikipedia['query']['search'][0]['snippet']
-st.write(textfromWikipedia)
-
-def strip_html(stringwithHTML):
-    mydefinition = str(html.fromstring(stringwithHTML).text_content())
-    return str(html.fromstring(stringwithHTML).text_content())
-
-cleanTextfromWikipedia = strip_html(textfromWikipedia)    
-st.write(cleanTextfromWikipedia)
