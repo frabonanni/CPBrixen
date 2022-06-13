bb2189bdb83660ea2088a78abdc663218f1d3b41
frabonanni <101397103+frabonanni@users.noreply.github.com>
2022-06-13 12:06:56 +0200

    Update project_prove.py

--- project/project_prove.py
+++ project/project_prove.py
@@ -24,84 +24,83 @@ st.markdown("""---""")
 option= st.selectbox('How would you like to import the text? Please select either browser or input', (' ','browser', 'input'))
 if option == 'browser':
    imported_file=st.file_uploader("Choose a text file")
-   stringio = StringIO(imported_file.getvalue().decode("utf-8"))
-   string_data = stringio.read()
-   st.write(string_data)
-   st.write(stringio)
-   
-   
-   srclang= 'it'
-   destlang= st.text_input('Tell me a two letter code for the destination language like fr or de: ', value= '')
-   option = st.selectbox(
-   'Here you can see some language codes to choose for your language',
-   ('en','fr','es', 'sq', 'ro', 'af', 'bs', 'mk', 'mt'))
-   if destlang is not None and imported_file is not None:
-      aaa=gTTS(text=imported_file, lang =srclang)
-      st.write('Press the play button below and listen to the audio file')
-      aaa.save('file_name.mp3')
-      audio_file= open('file_name.mp3', "rb")
-      st.audio(data=audio_file, format="audio/mp3", start_time=0)
-      st.download_button(label= "If you wish download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
-      st.markdown("""---""")
-      translator= Translator()
-      TranstoLan= translator.translate(imported_file, src=srclang, dest= destlang)
-      st.write('the translation is',TranstoLan.text)
-      url = 'https://it.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+imported_file
-      response = requests.get(url)
-      dataFromWikipedia = json.loads(response.text)
-      textfromWikipedia = dataFromWikipedia['query']['search'][0]['snippet']
-      cleanTextfromWikipedia = strip_html(textfromWikipedia)
-      st.write(cleanTextfromWikipedia)
-      translator= Translator()
-      TranstoLan= translator.translate(cleanTextfromWikipedia, src='it', dest= destlang)
-      desttranslate = TranstoLan.text
-      st.write('the translation is',desttranslate)
-      st.markdown("""---""")
+   if imported_file is not None:
+       stringio = StringIO(imported_file.getvalue().decode("utf-8"))
+       string_data = stringio.read()
+       st.write(string_data)
+       st.write(stringio)
+       srclang= 'it'
+       destlang= st.text_input('Tell me a two letter code for the destination language like fr or de: ', value= '')
+       option = st.selectbox(
+       'Here you can see some language codes to choose for your language',
+       ('en','fr','es', 'sq', 'ro', 'af', 'bs', 'mk', 'mt'))
+       if destlang is not None and imported_file is not None:
+          aaa=gTTS(text=imported_file, lang =srclang)
+          st.write('Press the play button below and listen to the audio file')
+          aaa.save('file_name.mp3')
+          audio_file= open('file_name.mp3', "rb")
+          st.audio(data=audio_file, format="audio/mp3", start_time=0)
+          st.download_button(label= "If you wish download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
+          st.markdown("""---""")
+          translator= Translator()
+          TranstoLan= translator.translate(imported_file, src=srclang, dest= destlang)
+          st.write('the translation is',TranstoLan.text)
+          url = 'https://it.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+imported_file
+          response = requests.get(url)
+          dataFromWikipedia = json.loads(response.text)
+          textfromWikipedia = dataFromWikipedia['query']['search'][0]['snippet']
+          cleanTextfromWikipedia = strip_html(textfromWikipedia)
+          st.write(cleanTextfromWikipedia)
+          translator= Translator()
+          TranstoLan= translator.translate(cleanTextfromWikipedia, src='it', dest= destlang)
+          desttranslate = TranstoLan.text
+          st.write('the translation is',desttranslate)
+          st.markdown("""---""")
 
-      yourtext=gTTS(text= desttranslate, lang=destlang)
-      yourtext.save('your_translation.mp3')
-      audio_file1= open('your_translation.mp3', "rb")
-      st.audio(data=audio_file1, format="audio/mp3", start_time=0)
+          yourtext=gTTS(text= desttranslate, lang=destlang)
+          yourtext.save('your_translation.mp3')
+          audio_file1= open('your_translation.mp3', "rb")
+          st.audio(data=audio_file1, format="audio/mp3", start_time=0)
 
-      st.write('the translation is',TranstoLan.text)
+          st.write('the translation is',TranstoLan.text)
 else:
     myword=st.text_input('Give me an Italian word to translate ',value='')
     srclang= 'it'
     destlang= st.text_input('Tell me a two letter code for the destination language like fr or de: ', value= '')
     option = st.selectbox(
     'Here you can see some language codes to choose for your language',
-    ('en','fr','es', 'sq', 'ro', 'af', 'bs', 'mk', 'mt'))
-    if destlang is not None and myword is not '':
-       firstword=gTTS(text=myword, lang =srclang)
-       st.write('Press the play button below and listen to the audio file')
-       firstword.save('file_name.mp3')
-       audio_file= open('file_name.mp3', "rb")
-       st.audio(data=audio_file, format="audio/mp3", start_time=0)
-       st.download_button(label= "If you wish download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
-       st.markdown("""---""")
-       translator= Translator()
-       TranstoLan= translator.translate(myword, src=srclang, dest= destlang)
-       st.write('the translation is',TranstoLan.text)
-       url = 'https://it.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+myword
-       response = requests.get(url)
-       dataFromWikipedia = json.loads(response.text)
-       textfromWikipedia = dataFromWikipedia['query']['search'][0]['snippet']
-       cleanTextfromWikipedia = strip_html(textfromWikipedia)
-       st.write(cleanTextfromWikipedia)
-       translator= Translator()
-       TranstoLan= translator.translate(cleanTextfromWikipedia, src='it', dest= destlang)
-       desttranslate = TranstoLan.text
-       st.write('the translation is',desttranslate)
-       st.markdown("""---""")
+     ('en','fr','es', 'sq', 'ro', 'af', 'bs', 'mk', 'mt'))
+     if destlang is not None and myword is not '':
+        firstword=gTTS(text=myword, lang =srclang)
+        st.write('Press the play button below and listen to the audio file')
+        firstword.save('file_name.mp3')
+        audio_file= open('file_name.mp3', "rb")
+        st.audio(data=audio_file, format="audio/mp3", start_time=0)
+        st.download_button(label= "If you wish download the audio file", data= audio_file, file_name="new_text_audio", mime="audio/mp3")
+        st.markdown("""---""")
+        translator= Translator()
+        TranstoLan= translator.translate(myword, src=srclang, dest= destlang)
+        st.write('the translation is',TranstoLan.text)
+        url = 'https://it.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='+myword
+        response = requests.get(url)
+        dataFromWikipedia = json.loads(response.text)
+        textfromWikipedia = dataFromWikipedia['query']['search'][0]['snippet']
+        cleanTextfromWikipedia = strip_html(textfromWikipedia)
+        st.write(cleanTextfromWikipedia)
+        translator= Translator()
+        TranstoLan= translator.translate(cleanTextfromWikipedia, src='it', dest= destlang)
+        desttranslate = TranstoLan.text
+        st.write('the translation is',desttranslate)
+        st.markdown("""---""")
 
-       yourtext=gTTS(text= desttranslate, lang=destlang)
-       yourtext.save('your_translation.mp3')
-       audio_file1= open('your_translation.mp3', "rb")
-       st.audio(data=audio_file1, format="audio/mp3", start_time=0)
+        yourtext=gTTS(text= desttranslate, lang=destlang)
+        yourtext.save('your_translation.mp3')
+        audio_file1= open('your_translation.mp3', "rb")
+        st.audio(data=audio_file1, format="audio/mp3", start_time=0)
 
-       st.write('the translation is',TranstoLan.text)
+        st.write('the translation is',TranstoLan.text)
 
-st.markdown("""---""")
+    st.markdown("""---""")
 
 st.write("Credits:")
 st.write("""
